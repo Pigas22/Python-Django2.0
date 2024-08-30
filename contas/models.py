842db03149b81f06d1169ad15b95ae90fcb1ba25
Pigas22 <tholzc@gmail.com>
2024-08-30 00:58:47 -0300

    Teorias e Aprendizados Aplicados - Aulas 8 à 12

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db import models as m
 
 # Create your models here.
+
+class Categoria(m.Model):
+    var_nome = m.CharField(max_length= 100)
+    var_descricao = m.CharField(max_length= 350)
+    dt_criacao = m.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return self.var_nome
+
+
+class Transacao(m.Model):
+    data = m.DateTimeField(auto_now= True)
+    descricao = m.CharField(max_length= 350)
+    valor = m.DecimalField(max_digits= 7, decimal_places= 2)
+    categoria = m.ForeignKey(Categoria, on_delete= m.CASCADE)
+    observacoes = m.TextField(null= True, blank= True)
+
+    class Meta:
+        verbose_name_plural = "Transacoes"
+
+    def __str__(self):
+        return self.descricao
